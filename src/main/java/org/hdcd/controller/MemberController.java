d9a95c27a5d1fc0507b02ff4fedaa52230e9c2c7
2000503 <0117Qwer!>
2021-08-27 16:35:15 +0900

    test

--- src/main/java/org/hdcd/controller/MemberController.java
+++ src/main/java/org/hdcd/controller/MemberController.java
@@ -12,6 +12,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -87,12 +91,51 @@ public class MemberController {
         log.info("original filename {}", originalFileName);
 
         Path copyOfLocation = Paths.get(uploadDir + File.separator + StringUtils.cleanPath(file.getOriginalFilename()));
+        log.info("copyOfLocation {}", copyOfLocation);
         try {
             Files.copy(file.getInputStream(), copyOfLocation, StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
         ResponseEntity<String> entity = new ResponseEntity<>("UPLOAD SUCCESS " + originalFileName, HttpStatus.OK);
+        return entity;
+    }
+
+    @PostMapping(value ="/register11", produces = "text/plain;charset=UTF-8")
+    public ResponseEntity<String> register11(@Validated @RequestBody Member member, BindingResult bindingResult) {
+        log.info( "result error = {}", bindingResult.hasErrors());
+
+        if ( bindingResult.hasErrors() ) {
+            List<ObjectError> allErrors = bindingResult.getAllErrors();
+            List<ObjectError> globalErrors = bindingResult.getGlobalErrors();
+            List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+            log.info("allErrors.size {} ", allErrors.size());
+            log.info("globalErrors.size {} ", globalErrors.size());
+            log.info("fieldErrors.size {} ", fieldErrors.size());
+
+            for (ObjectError objectError: allErrors) {
+                log.info("objectError {}", objectError);
+            }
+            for (ObjectError globalError: globalErrors) {
+                log.info("globalErrors {}", globalErrors);
+            }
+            for (FieldError fieldError: fieldErrors) {
+                log.info("fieldError {}", fieldError);
+                log.info("fieldError message {}", fieldError.getDefaultMessage());
+            }
+
+            ResponseEntity<String> entity = new ResponseEntity<>("SUCCESS", HttpStatus.BAD_REQUEST);
+            return entity;
+        }
+        log.info("getUserId {}", member.getUserId());
+        log.info("getPassword {}", member.getPassword());
+        log.info("getUserName {}", member.getUserName());
+        log.info("getEmail {}", member.getEmail());
+        log.info("getDateOfBirth {}", member.getDateOfBirth());
+        log.info("getGender {}", member.getGender());
+
+        ResponseEntity<String> entity = new ResponseEntity<>("SUCCESS", HttpStatus.OK);
         return entity;
     }
 }
--- src/main/java/org/hdcd/domain/Member.java
+++ src/main/java/org/hdcd/domain/Member.java
@@ -2,19 +2,40 @@ package org.hdcd.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @ToString
 public class Member {
-    private String userId = "spider";
-    private String password = "1234";
 
-    @JsonFormat(pattern = "yyyyMMdd")
+//    private String userId = "spider";
+//    prlivate String password = "1234";
+    @NotBlank
+    private String userId;
+
+    @NotBlank
+    @Size(max=3)
+    private String userName;
+
+    private String password;
+    @Email
+    private String email;
+
+    private String gender;
+
+    // 과거날짜인지 검사
+    @Past
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate dateOfBirth;
 
     private Address address;
